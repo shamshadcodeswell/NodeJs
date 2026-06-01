958ac71d0ab80979985215d8c7fdfcb0e533dae0
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-06-01 22:42:48 +0530

    http inbuilt module

---
+++ 7-https.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server= http.createServer((req,res)=>{
+    if(req.url=='/'){
+        res.end('Welcome to our homepage')
+    }
+    else if(req.url=='/about'){
+        res.end('Here is a short intro of ours')
+    }
+    else {res.end(`
+       <h1>Oops!</h1>
+       <p>cant find that !</p> 
+       <a href="/">back home</a>
+       `
+    )
+    }
+})
+server.listen(5000)
