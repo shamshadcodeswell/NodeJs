ad45f82d85ef6379a99643ea1982b11c46d16f84
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-06-01 21:33:49 +0530

    path and fs inbuilt modules

---
+++ 5-fs-sync.js
@@ -0,0 +1,13 @@
+const {readFileSync, writeFileSync} = require('fs');
+
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
+
+console.log(first, second);
+
+writeFileSync(
+    './content/result-fs-sync.txt',
+     `this is the result : ${first} ${second} `,
+     {flag:'a'}
+
+)
